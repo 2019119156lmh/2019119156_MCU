3974fe7af520f9a84363121a0ca67371be510f11
lmh <liao010501@163.com>
2021-12-28 13:19:05 +0800

    实验一

---
+++ 1实验一-GPIO 控制小熊派 LED1/Inc/gpio.h
@@ -0,0 +1,62 @@
+/**
+  ******************************************************************************
+  * File Name          : gpio.h
+  * Description        : This file contains all the functions prototypes for 
+  *                      the gpio  
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __gpio_H
+#define __gpio_H
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* USER CODE BEGIN Includes */
+typedef enum
+{
+	LED_OFF=0,
+	LED_ON,
+	LED_Toggle
+}LED_ENUM;
+/* USER CODE END Includes */
+
+/* USER CODE BEGIN Private defines */
+#define LED_Set(status) status != LED_Toggle ? HAL_GPIO_WritePin(LED_GPIO_Port,LED_Pin, status != LED_ON ? GPIO_PIN_RESET : GPIO_PIN_SET):HAL_GPIO_TogglePin(LED_GPIO_Port,LED_Pin);
+/* USER CODE END Private defines */
+
+void MX_GPIO_Init(void);
+
+/* USER CODE BEGIN Prototypes */
+
+/* USER CODE END Prototypes */
+
+#ifdef __cplusplus
+}
+#endif
+#endif /*__ pinoutConfig_H */
+
+/**
+  * @}
+  */
+
+/**
+  * @}
+  */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
